f33da15a642e1c0749219ff1073b1a21c1f5c338
Sedat CIFTCI <me@sedat.us>
2015-11-25 18:32:40 +0200

    Storyboard Support

--- example/delegate_example/app.cr
+++ example/delegate_example/app.cr
@@ -20,25 +20,36 @@ class MyView < NSView
 	export "example_button_action", "exampleButtonAction"
 end
 
-class ButtonHandler < NSObject
-	export_class
-  def example_button_action
-    ns_log "button clicked"
-  end
-  export "example_button_action", "exampleButtonAction"
-end
 
 class ViewController < NSViewController
 	export_class
+	@@username_text_field
+	@@password_text_field
+
 	def view_did_load
 		view = self.view as NSView
+
+		@@username_text_field = view.view_with_tag = 1
+		@@username_text_field = @@username_text_field as NSTextField
+
+		@@password_text_field = view.view_with_tag = 2
+		@@password_text_field = @@password_text_field as NSTextField
+
 		sad = view.view_with_tag = 123
 		sad = sad as NSButton
-		sad.target = ButtonHandler.new.to_objc
+		sad.target = self.to_objc
 		sad.action = "exampleButtonAction".to_sel.to_objc
 	end
 
+	def example_button_action
+		username_text_field = @@username_text_field as NSTextField
+		password_text_field = @@password_text_field as NSTextField
+		
+		ns_log "button_clicked!!1"
+	end
+
 	export "view_did_load", "viewDidLoad"
+	export "example_button_action", "exampleButtonAction"
 end
 
 class MyAppDelegate < NSObject
