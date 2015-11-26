53bc61ba3aac0745e41129c2aac61cd7a5fc8cb2
Sedat CIFTCI <me@sedat.us>
2015-11-27 00:55:11 +0200

    icns

--- example/delegate_example/build.sh
+++ example/delegate_example/build.sh
@@ -4,10 +4,9 @@ rm -rf Hoop.app
 mkdir -p Hoop.app/Contents/MacOS
 mkdir -p Hoop.app/Contents/Resources/Base.lproj
 
-#ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Application.nib Application.xib
-ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Main.storyboardc Main.storyboard
-cp Info.plist Hoop.app/Contents/
+#ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Application.nib supporting_files/Application.xib
+ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Main.storyboardc supporting_files/Main.storyboard
+cp supporting_files/Info.plist Hoop.app/Contents/
 cp assets/* Hoop.app/Contents/Resources
-cp Icon.icns Hoop.app/Contents/Resources
 
 crystal build app.cr -o Hoop.app/Contents/MacOS/app
