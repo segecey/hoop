6201821b1c1b6e0037084012d56f866823b3cc2a
Sedat CIFTCI <me@sedat.us>
2015-11-27 00:51:13 +0200

    icns

--- example/delegate_example/build.sh
+++ example/delegate_example/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-rm -rf Sample.app
-mkdir -p Sample.app/Contents/MacOS
-mkdir -p Sample.app/Contents/Resources/Base.lproj
+rm -rf Hoop.app
+mkdir -p Hoop.app/Contents/MacOS
+mkdir -p Hoop.app/Contents/Resources/Base.lproj
 
-#ibtool --errors --warnings --notices --output-format human-readable-text --compile Sample.app/Contents/Resources/Base.lproj/Application.nib Application.xib
-ibtool --errors --warnings --notices --output-format human-readable-text --compile Sample.app/Contents/Resources/Base.lproj/Main.storyboardc Main.storyboard
-cp Info.plist Sample.app/Contents/
-cp assets/* Sample.app/Contents/Resources
+#ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Application.nib Application.xib
+ibtool --errors --warnings --notices --output-format human-readable-text --compile Hoop.app/Contents/Resources/Base.lproj/Main.storyboardc Main.storyboard
+cp Info.plist Hoop.app/Contents/
+cp assets/* Hoop.app/Contents/Resources
+cp Icon.icns Hoop.app/Contents/Resources
 
-crystal build app.cr -o Sample.app/Contents/MacOS/app
+crystal build app.cr -o Hoop.app/Contents/MacOS/app
