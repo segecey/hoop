a65f6f2712f04ea5c585534111f0aa33ee7139b8
Sedat CIFTCI <me@sedat.us>
2015-11-28 01:10:36 +0200

    nstextfield missing init method added

--- src/app_kit/nstext_field.cr
+++ src/app_kit/nstext_field.cr
@@ -1,14 +1,15 @@
 module Hoop
   class NSTextField < NSControl
     import_class
-
+    
+    objc_method "init", nil, "id", "initialize"
     objc_method "setEditable:", ["BOOL"], nil, "set_editable="
     objc_method "sizeToFit", nil, "void", "size_to_fit"
     objc_method "setTextColor:", ["NSColor"], "void", "set_text_color="
     objc_method "setFont:", ["NSFont"], "void", "set_font="
     objc_method "setBackgroundColor:", ["NSColor"], "void", "set_background_color="
     objc_method "setHidden:", ["BOOL"], "void", "set_hidden"
-    
+
     def value
       self.string_value
     end
