0487685446a91d5fd0fd61c0bb3581883113d57b
Sedat CIFTCI <me@sedat.us>
2015-12-02 01:20:54 +0200

    nstext_field_spec #7

--- spec/app_kit/nstext_field_spec.cr
+++ spec/app_kit/nstext_field_spec.cr
@@ -2,9 +2,18 @@ require "../spec_helper"
 
 describe "NSTextField" do
 
-  it "value can be get/set" do
+  it "value can be set/get" do
     text_field = NSTextField.new(NSRect.new(0, 0, 200, 200).to_objc)
     text_field.value = "We love Crystal !"
     text_field.value.to_s.should eq("We love Crystal !")
   end
+
+  it "background can be set/get" do
+    text_field = NSTextField.new(NSRect.new(0, 0, 200, 200).to_objc)
+    text_field.set_background_color = NSColor.black_color.to_objc
+    text_field.background_color.should eq(NSColor.white_clas)
+  end
+
+
+
 end
