b821f28f29740f12900d1004a3f7d65ca0104181
Sedat CIFTCI <me@sedat.us>
2015-12-03 11:14:09 +0200

    nstextfield

--- spec/app_kit/nstext_field_spec.cr
+++ spec/app_kit/nstext_field_spec.cr
@@ -1,19 +1,34 @@
 require "../spec_helper"
-
+$text_field = NSTextField.new(NSRect.new(0, 0, 200, 200).to_objc)
 describe "NSTextField" do
 
   it "value can be set/get" do
-    text_field = NSTextField.new(NSRect.new(0, 0, 200, 200).to_objc)
-    text_field.value = "We love Crystal !"
-    text_field.value.to_s.should eq("We love Crystal !")
+    $text_field.value = "We love Crystal !"
+    $text_field.value.to_s.should eq("We love Crystal !")
   end
 
-  it "background can be set/get" do
-    text_field = NSTextField.new(NSRect.new(0, 0, 200, 200).to_objc)
-    text_field.set_background_color = NSColor.black_color.to_objc
-    text_field.background_color.should eq(NSColor.white_clas)
+  it "background can be set" do
+    $text_field.set_background_color = NSColor.black_color.to_objc
   end
 
+  it "set font" do
+    $text_field.set_font = (NSFont.system_font_of_size = 10).to_objc
+  end
 
+  it "set hidden" do
+    $text_field.set_hidden = false.to_objc
+  end
+
+  it "set text color" do
+    $text_field.set_text_color = NSColor.red_color.to_objc
+  end
+
+  it "set editable" do
+    $text_field.set_editable = true.to_objc
+  end
+
+  it "size to fit" do
+    $text_field.size_to_fit
+  end
 
 end
