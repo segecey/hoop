860014c033c6e52d8eb00ad223dd0407353a2653
Sedat CIFTCI <me@sedat.us>
2015-12-07 00:48:13 +0200

    IOBluetooth framework example added

---
+++ example/paired_bluetooth_device.cr
@@ -0,0 +1,10 @@
+require "./../src/hoop"
+include Hoop
+
+a = IOBluetoothDevice.paired_devices as NSArray
+i = 0
+while i < a.count
+  sel = (a.object_at_index = i) as IOBluetoothDevice
+  puts sel.get_name
+  i += 1
+end
