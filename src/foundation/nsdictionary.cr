e07da3f1a016f6e16b3bf6fb6879dc9116d54557
Sedat G. ÇİFTÇİ <me@sedat.us>
2016-10-25 15:28:22 +0300

    clean

--- src/foundation/nsdictionary.cr
+++ src/foundation/nsdictionary.cr
@@ -4,9 +4,5 @@ module Hoop
   class NSDictionary < NSObject
     register_class
     objc_method "objectForKey:", ["NSString"], "id", "object_for_key"
-    def test
-    	val = self.object_for_key "userId"
-    	klass = NSClass.new(LibObjC.objc_msgSend(val.to_objc, "class".to_sel.to_objc).as(LibObjC::Class))
-    end
   end
 end
